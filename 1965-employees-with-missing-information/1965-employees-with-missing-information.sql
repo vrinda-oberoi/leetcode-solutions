48b53f079825338aca916ea3ae6ac8f89d4b1341
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-08-07 23:16:25 +0530

    Time: 596 ms (57.63%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select e.employee_id from Employees e
+LEFT JOIN Salaries s
+ON e.employee_id = s.employee_id 
+WHERE s.salary is NULL
+UNION
+select s.employee_id from Salaries s
+LEFT JOIN Employees e
+ON e.employee_id = s.employee_id 
+WHERE e.name is NULL
+
+ORDER BY employee_id;

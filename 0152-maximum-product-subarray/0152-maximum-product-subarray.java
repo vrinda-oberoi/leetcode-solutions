c7e2f77df1c422dec91de79c7d85b52f41b910c8
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-08-11 21:00:44 +0530

    Time: 1 ms (92.95%), Space: 47.1 MB (88.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,12 +3,22 @@ class Solution {
         int n = nums.length;
         int largest = Integer.MIN_VALUE;
 
+        int prefix = 1;
+        int suffix = 1;
+
         for(int i=0;i<n;i++){
-            int product = 1;
-            for(int j =i;j<n;j++){
-                product *= nums[j];
-                largest = Math.max(largest,product);
+            if(prefix == 0){
+                prefix =1;
             }
+
+            if(suffix == 0){
+                suffix =1;
+            }
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            largest = Math.max(largest,Math.max(prefix,suffix));
         }
         return largest;
     }

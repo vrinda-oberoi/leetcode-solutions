947716f4e8ec5f2070a9de5d1e665649165f6de5
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-08-30 15:23:13 +0530

    Time: 451 ms (53.42%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-Select p.firstName,p.lastName,a.city,a.state from Person as p  LEFT JOIN Address as a ON p.personId = a.personID;
+Select 
+p.firstName,p.lastName,a.city,a.state 
+from Person as p  
+LEFT JOIN 
+Address as a 
+ON 
+p.personId = a.personID;

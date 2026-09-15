4018bd5c7e945e1bbc41b95aa51751bd36a66d39
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-09-15 09:39:41 +0530

    Time: 562 ms (10.59%), Space: 275.9 MB (5.17%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,45 +1,18 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        int n = s.length();
-        if(n <10){
-            return new ArrayList<>();
+        List<String>  ans = new ArrayList<>();
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for(int i=0;i<=s.length()-10;i++){
+            String sub = s.substring(i,i+10);
+            map.put(sub,map.getOrDefault(sub,0)+1);
         }
 
-        //map the characters to 2 bit since we are having only four characters
-        int map[] = new int[256];
-        map['A'] = 0;
-        map['C'] = 1;
-        map['G'] = 2;
-        map['T'] = 3;
-
-        int base = 4;
-        int L =10;
-
-        int baseL = (int) Math.pow(base,L-1);
-
-        Set<Integer> found = new HashSet<>();
-        Set<String> result = new HashSet<>();
-
-        int rollingHash = 0;
-        
-        for(int i=0;i<L;i++){
-            rollingHash =rollingHash *base +map[s.charAt(i)];
-        }
-        found.add(rollingHash);
-
-        for(int i=L;i<n;i++){
-            int outGoing =map[s.charAt(i-L)];
-            rollingHash -= outGoing *baseL;
-
-            int incomingVal = map[s.charAt(i)];
-            rollingHash = rollingHash * base + incomingVal;
-
-            if (!found.add(rollingHash)) {
-                result.add(s.substring(i - L + 1, i + 1));
+        for(String str : map.keySet()){
+            if(map.get(str) > 1){
+                ans.add(str);
             }
         }
-
-        return new ArrayList<>(result);
-
+        return ans;
     }
 }

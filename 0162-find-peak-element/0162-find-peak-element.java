ac825eae28c04af8bc5c962a03dc13d8f0a28743
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-09-21 22:06:39 +0530

    Time: 0 ms (100%), Space: 44.5 MB (6.18%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,18 +2,22 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
         int low = 0;
-        int high =n-1;
+        int high = n-1;
+
+        if(n == 1){
+            return 0;
+        }
+
 
         while(low <high){
             int mid = low+(high-low)/2;
-
-            if(nums[mid] <nums[mid+1]){
-                //mtlb right mae peak milegi kyuki increase ho rha hai 
-                low=mid+1;
+            if(nums[mid] < nums[mid+1]){
+                low = mid+1;
             }else{
-                high=mid;
+                high = mid;
             }
         }
+
         return low;
     }
 }

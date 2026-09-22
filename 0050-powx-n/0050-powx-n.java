b2fc9b16539197301ee6fa39813abbeabc701166
VRINDA OBEROI <vrindaoberoi251@gmail.com>
2026-09-22 22:44:07 +0530

    Time: 1 ms (9.18%), Space: 48 MB (45.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,6 +2,25 @@ import java.util.*;
 class Solution {
 
     public double myPow(double x, int n) {
-       return Math.pow(x,n);
+       long N =n;
+       if(N<0){
+        x = 1/x;
+        N = -N;
+       }
+       return fastPow(x,N);
+    }
+
+    private double fastPow(double x,long n){
+        if(n == 0){
+            return 1.0;
+        }
+
+        double half = fastPow(x,n/2);
+
+        if(n %2 == 0){
+            return half*half;
+        }else{
+            return x * half *half;
+        }
     }
 }
